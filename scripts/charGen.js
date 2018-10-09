66a3daeb2065b2bb6c8d2418e5d7161715918cd3
Meepedy <david@psyon.org>
2018-10-09 10:36:20 -0500

    character constructor

---
+++ scripts/charGen.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var app = app || {};
+var data = data || {};
+
+function Character() {
+    this.hairColor = app.randomItem(data.hairColors);
+    this.hairStyle = app.randomItem(data.hairStyles);
+    this.eyeColor = app.randomItem(data.eyeColors);
+    this.faceShape = app.randomItem(data.faceShapes);
+    this.race = app.randomItem(data.races);
+    this.gender = app.randomItem(data.genders);
+    this.orientation = app.randomItem(data.orientations);
+    this.attitude = app.randomItem(data.attitudes);
+    this.hobby = app.randomItem(data.hobbies);
+    this.traits = [app.randomItem(data.traits), app.randomItem(data.traits)];
+    this.motivation = app.randomItem(data.motivations);
+
+}
