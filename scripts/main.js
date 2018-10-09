772a936bb915d5cde9c42bb0eec4f02b39f41196
Meepedy <david@psyon.org>
2018-10-09 09:57:45 -0500

    put main.js functions inside a module

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,27 @@
 'use strict';
 
-function displayCharacter(char) {
-    console.log(`Hair: ${char.hairColor}, ${char.hairStyle}`);
-    console.log(`Eyes: ${char.eyeColor}`);
-    console.log(`Face Shape: ${char.faceShape}`);
-    console.log(`Race: ${char.race}`);
-    console.log(`Gender: ${char.gender}`);
-    console.log(`Orientation: ${char.orientation}`);
-    console.log(`Attitude: ${char.attitude}`);
-    console.log('Traits: ');
-    for (trait in char.psychologicalTraits) {
-        console.log(`- ${trait}`);
+var app = app || {}
+
+(function(module) {
+    module.displayCharacter = function(char) {
+        console.log(`Hair: ${char.hairColor}, ${char.hairStyle}`);
+        console.log(`Eyes: ${char.eyeColor}`);
+        console.log(`Face Shape: ${char.faceShape}`);
+        console.log(`Race: ${char.race}`);
+        console.log(`Gender: ${char.gender}`);
+        console.log(`Orientation: ${char.orientation}`);
+        console.log(`Attitude: ${char.attitude}`);
+        console.log('Traits: ');
+        for (trait in char.psychologicalTraits) {
+            console.log(`- ${trait}`);
+        }
+        console.log(`Hobby: ${char.hobby}`);
+        console.log(`Motivation: ${char.motivation}`);
     }
-    console.log(`Hobby: ${char.hobby}`);
-    console.log(`Motivation: ${char.motivation}`);
-}
-
-function main() {
-    let character = new Character();
-
-    displayCharacter(character);
-}
+    
+    module.main = function() {
+        let character = new Character();
+    
+        displayCharacter(character);
+    }
+})(app);
