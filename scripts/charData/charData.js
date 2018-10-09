46830b16f9ac62ea1d79d6fc3fd4526083d59332
Meepedy <david@psyon.org>
2018-10-09 10:23:47 -0500

    filled out some character data to use during generation

---
+++ scripts/charData/charData.js
@@ -0,0 +1,115 @@
+'use strict';
+
+var data = data || {};
+
+(function(module){
+
+    module.hairColors = [
+        'black',
+        'brown',
+        'auburn',
+        'blonde',
+        'red',
+        'jet black',
+        'dark brown',
+        'light brown',
+        'white',
+        'grey',
+     ];
+
+     module.hairStyles = [
+         'bald',
+         'short-cropped',
+         'shoulder-length',
+         'long',
+         'spikey',
+         'wavey',
+         'cornrows',
+         'bobbed',
+         'ponytail',
+         'topknot',
+         'wait-length',
+     ];
+
+     module.eyeColors = [
+         'amber',
+         'blue',
+         'brown',
+         'hazel',
+         'green',
+         'emerald',
+         'silver',
+     ];
+
+     module.faceShapes = [
+         'oval',
+         'thin',
+         'round',
+         'chiseled',
+         'square',
+     ];
+
+     module.races = [
+         'human',
+         'elf',
+         'dwarf',
+         'goblin',
+         'orc',
+     ];
+
+     module.genders = [
+         'male',
+         'female',
+         'none',
+     ];
+
+     module.orientation = [
+         'straight',
+         'gay',
+         'bi',
+     ];
+
+     module.attitudes = [
+         'confident',
+         'contempuous',
+         'devious',
+         'proud',
+         'optimistic',
+         'pessimistic',
+         'encouraging',
+         'fair',
+         'just',
+         'religious',
+     ];
+
+     module.hobbies = [
+         'whittling',
+         'carving',
+         'darts',
+         'tournaments',
+         'fishing',
+         'hunting',
+         'painting',
+     ];
+
+     module.traits = [
+         'arrogant',
+         'anxious',
+         'envious',
+         'stable',
+         'brave',
+         'trusting',
+         'meticulous',
+         'fierce',
+     ];
+
+     module.motivations = [
+         'money',
+         'power',
+         'fame',
+         'family',
+         'friends',
+         'revenge',
+     ];
+
+})(data);
