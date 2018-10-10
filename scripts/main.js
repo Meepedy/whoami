24bc0bee9b86ff9792ab371a2723ff09187f8043
Meepedy <david@psyon.org>
2018-10-10 09:46:07 -0500

    appends to DOM when character is generated so that it displays on the page and not just the console

--- scripts/main.js
+++ scripts/main.js
@@ -19,6 +19,20 @@ var app = app || {};
     });
     console.log(`Hobby: ${char.hobby}`);
     console.log(`Motivation: ${char.motivation}`);
+
+    document.getElementById('hair').appendChild(document.createTextNode(`${char.hairColor}, ${char.hairStyle}`));
+    document.getElementById('eyes').appendChild(document.createTextNode(`${char.eyeColor}`));
+    document.getElementById('faceShape').appendChild(document.createTextNode(`${char.faceShape}`));
+    document.getElementById('height').appendChild(document.createTextNode(`${formatHeight(char.height)}`));
+    document.getElementById('weight').appendChild(document.createTextNode(`${char.weight}lbs`));
+    document.getElementById('race').appendChild(document.createTextNode(`${char.race}`));
+    document.getElementById('gender').appendChild(document.createTextNode(`${char.gender}`));
+    document.getElementById('orientation').appendChild(document.createTextNode(`${char.orientation}`));
+    document.getElementById('attitude').appendChild(document.createTextNode(`${char.attitude}`));
+    document.getElementById('trait1').innerText = `- ${char.traits[0]}`;
+    document.getElementById('trait2').innerText =`- ${char.traits[1]}`;
+    document.getElementById('hobby').appendChild(document.createTextNode(`${char.hobby}`));
+    document.getElementById('motivation').appendChild(document.createTextNode(`${char.motivation}`));
   };
 
   function formatHeight(height) {
