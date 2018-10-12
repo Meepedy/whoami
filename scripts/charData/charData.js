e9b0d8f87f2bb859c2dfafe3e3ca701d2c77d670
Mattias Eklöf <mattias.eklof@gmail.com>
2018-10-12 21:40:30 +0200

    Make height depend on race and weight depend on height and bmi

--- scripts/charData/charData.js
+++ scripts/charData/charData.js
@@ -58,11 +58,81 @@ var data = data || {};
   ];
 
   module.races = [
-    'human',
-    'elf',
-    'dwarf',
-    'goblin',
-    'orc'
+    {
+      name: 'human',
+      minHeight: {
+        "male" : 5*12,
+        "female": 4*12+7,
+        "none": 4*12+7
+      },
+      maxHeight: {
+        "male": 6*12+6,
+        "female": 6*12+1,
+        "none": 6*12+6
+      },
+      minBmi: 18,
+      maxBmi: 30
+    },
+    {
+      name: 'elf',
+      minHeight: {
+        "male" : 5*12+3,
+        "female": 4*12+11,
+        "none": 4*12+11
+      },
+      maxHeight: {
+        "male": 6*12+1,
+        "female": 5*12+9,
+        "none": 6*12+1
+      },
+      minBmi: 16,
+      maxBmi: 25
+    },
+    {
+      name: 'dwarf',
+      minHeight: {
+        "male" : 3*12+11,
+        "female": 3*12+9,
+        "none": 3*12+9
+      },
+      maxHeight: {
+        "male": 4*12+5,
+        "female": 4*12+3,
+        "none": 4*12+5
+      },
+      minBmi: 18,
+      maxBmi: 30
+    },
+    {
+      name: 'goblin',
+      minHeight: {
+        "male" : 2*12+9,
+        "female": 2*12+7,
+        "none": 2*12+7
+      },
+      maxHeight: {
+        "male": 3*12+3,
+        "female": 3*12+1,
+        "none": 3*12+3
+      },
+      minBmi: 16,
+      maxBmi: 25
+    },
+    {
+      name: 'orc',
+      minHeight: {
+        "male" : 6*12+8,
+        "female": 6*12+2,
+        "none": 6*12+2
+      },
+      maxHeight: {
+        "male": 8*12+6,
+        "female": 8*12,
+        "none": 8*12+6
+      },
+      minBmi: 25,
+      maxBmi: 41
+    }
   ];
 
   module.genders = [
--- scripts/utility.js
+++ scripts/utility.js
@@ -6,4 +6,7 @@ var app = app || {};
     let randomIndex = Math.floor(Math.random() * Math.floor(array.length));
     return array[randomIndex];
   };
+  module.calcWeight = function(heightInInches, bmi) {
+    return Math.floor(Math.pow(heightInInches, 2)*bmi/703);
+  };
 })(app);
