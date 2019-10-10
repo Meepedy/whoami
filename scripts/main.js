74f5d90e6aeaf8af7633102820128df0fbdc21bd
Meepedy <david@psyon.org>
2019-10-09 21:36:18 -0500

    removed console logs

--- scripts/main.js
+++ scripts/main.js
@@ -4,23 +4,6 @@ var app = app || {};
 
 (function(module){
   module.displayCharacter = function(char) {
-    console.log(`Hair: ${char.hairColor}, ${char.hairStyle}`);
-    console.log(`Eyes: ${char.eyeColor}`);
-    console.log(`Face Shape: ${char.faceShape}`);
-    console.log(`Height: ${formatHeight(char.height)}`);
-    console.log(`Weight: ${char.weight}lbs`);
-    console.log(`Race: ${char.race}`);
-    console.log(`Gender: ${char.gender}`);
-    console.log(`Features: ${char.feature}`);
-    console.log(`Orientation: ${char.orientation}`);
-    console.log(`Attitude: ${char.attitude}`);
-    console.log('Traits: ');
-    char.traits.forEach(trait => {
-      console.log(`- ${trait}`);
-    });
-    console.log(`Hobby: ${char.hobby}`);
-    console.log(`Motivation: ${char.motivation}`);
-
     document.getElementById('hair').innerText = `${char.hairColor}, ${char.hairStyle}`;
     document.getElementById('eyes').innerText = `${char.eyeColor}`;
     document.getElementById('faceShape').innerText = `${char.faceShape}`;
